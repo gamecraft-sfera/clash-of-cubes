ce616f4e54e6d15fc4fae69d07d2b42bd5bcaa51
Žák VR 11 <zak.vr.11@sferapardubice.eu>
2025-12-02 18:07:23 +0100

    darek

--- vánoce/node_2d.gd
+++ vánoce/node_2d.gd
@@ -5,3 +5,12 @@ func _on_texture_button_pressed() -> void:
 	print("hej")
 	$TextureButton.visible = false
 	$"PixilFrame0(2)".visible = true
+
+
+
+
+func _on_texture_button_2_pressed() -> void:
+	print("a gun!")
+	$TextureButton2.visible = false
+	$"PixilFrame0(5)".visible = true
+   
