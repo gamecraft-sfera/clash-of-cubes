7218734927a506b9a0a9c0c4608b1520ec7ebf4a
Žák VR 11 <zak.vr.11@sferapardubice.eu>
2026-03-10 16:42:06 +0100

    arfafg
    gdwgz

--- angels-hell/sip.gd
+++ angels-hell/sip.gd
@@ -4,12 +4,11 @@ extends Area3D
 @export var speed: float = 0.5
 
 func _physics_process(delta: float) -> void:
-	var direction =global_position - target_position
+	var direction = target_position - global_position
 	direction.normalized()
 	global_position = global_position + (direction * speed * delta)
 	
+	look_at(target_position)
+	
 	if global_position.distance_to(target_position) <= 0.1:
 		queue_free()
-
-
-	queue_free()
